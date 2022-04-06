e029fef9b00b43ec36bf0c31f0921d53126cbecf
TheoDosSantos <102948361+TheoDosSantos@users.noreply.github.com>
2022-04-06 18:05:51 +0200

    Add files via upload

---
+++ Front/script/js/app.js
@@ -0,0 +1,65 @@
+window.addEventListener('load',() => {
+
+    /* ------------ HEADER ------------ */
+
+    let header = document.getElementsByTagName('header');
+    window.addEventListener('scroll', () => {
+        const scrolled = window.scrollY;
+        const scrollBegin = window.innerHeight-800;
+        if(scrolled > scrollBegin){
+            header[0].classList.add("style-scroll");
+        }
+        else{
+            header[0].classList.remove("style-scroll");
+        }
+    });
+
+    /* ------------ SECTION 5 ------------ */
+
+    let cards = document.querySelectorAll(".card-text");
+    let cardImg = document.querySelectorAll(".card-img")[0].childNodes[1];
+    let cardTxtA=document.getElementsByClassName("active-card");
+    console.log(cardImg);
+    cards.forEach(function(element){
+        element.addEventListener("click", function(){
+            if(this.classList.contains("active-card")){
+            }
+            else{
+                cardTxtA[0].classList.remove("active-card");
+                this.classList.add("active-card");
+                if(this.classList.contains("1")){
+                    cardImg.setAttribute("src","img/illu-s5-1.svg");
+                }
+                else if(this.classList.contains("2")){
+                    cardImg.setAttribute("src","img/illu-s5-2.svg");
+                }
+                else{
+                    cardImg.setAttribute("src","img/illu-s5-3.svg");
+                }
+            }
+        });
+    });
+
+    /* ------------ SECTION 7 ------------ */
+
+    let prev=document.getElementById("slide-prev");
+	let	next=document.getElementById("slide-next");
+    let divImg=document.getElementsByClassName("slide-img");
+    let divTxt=document.getElementsByClassName("slide-text");
+    let c = 0;
+    function changeSlide(m) {
+        if (m >= divImg.length) {m = 0;}
+        if (m < 0) {m = divImg.length - 1;}
+        divImg[c].classList.remove("active-img");
+        divTxt[c].classList.remove("active-text");
+        divImg[m].classList.add("active-img");
+        divTxt[m].classList.add("active-text");
+        c = m;
+    }
+    next.addEventListener("click", () => {
+        changeSlide(c + 1)
+    });
+    prev.addEventListener("click", () => {
+        changeSlide(c - 1)
+    });
+});
