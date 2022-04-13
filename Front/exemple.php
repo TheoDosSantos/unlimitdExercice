cfada663e3a2b30d10a6392192ab8aba018857fb
TheoDosSantos <102948361+TheoDosSantos@users.noreply.github.com>
2022-04-13 16:05:58 +0200

    Add files via upload

---
+++ Front/exemple.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+
+		<title>Exemple</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/exemple.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-devtools.php') ?>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+			
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/exemple-min.js"></script>
+	</body>
+</html>
